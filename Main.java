cf5ab218b6bacff089c0af4b47f1e44ee02bc1e2
shikhar-batham <83720643+shikhar-batham@users.noreply.github.com>
2024-08-15 12:14:42 +0530

    Update Main.java
    initial commit

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
-class Main{
-    public static void main(String[] args)
-    {
+class Main {
+    public static void main(String[] args) {
         System.out.println("Hello There");
+        System.out.println("Next print");
     }
 }
